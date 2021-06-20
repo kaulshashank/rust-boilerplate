349273e4c774eea13773ad6ea40cb47c6a3a1a7c
Shashank Kaul <kaulshashank96@gmail.com>
2021-06-20 13:33:21 +0530

    dir printing

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,9 @@ mod print;
 fn main() {
     let paths = file_io::read_dir(".");
     for path in paths {
-        println!("Path is: {}", path.display())
+        if path.is_dir() {
+            println!("\n\nDirectories in this path are:");
+            print::display_dir(path);
+        }
     }
 }
--- src/print.rs
+++ src/print.rs
@@ -1,34 +1,42 @@
-use std::io;
+use std::path::PathBuf;
 
-fn give_two_words() -> (String, String) {
-    (format!("fellow"), format!("Rustaceans"))
+pub fn display_dir(path: PathBuf) {
+    let str = path.into_os_string().into_string().unwrap();
+    let mut chars = str.chars();
+    chars.next();
+    chars.next();
+    println!("* {}", chars.as_str());
 }
 
-fn count_characters(name: String) -> i32 {
-    let mut count = 0;
-    for _ in name.chars() {
-        count += 1;
-    }
-    count
-}
+// fn give_two_words() -> (String, String) {
+//     (format!("fellow"), format!("Rustaceans"))
+// }
 
-fn remove_vowels(name: String) -> String {
-    let mut output = String::new();
-    for c in name.chars() {
-        match c {
-            'a' | 'e' | 'i' | 'o' | 'u' => {}
-            _ => {
-                output.push(c);
-            }
-        }
-    }
-    output
-}
+// fn count_characters(name: String) -> i32 {
+//     let mut count = 0;
+//     for _ in name.chars() {
+//         count += 1;
+//     }
+//     count
+// }
 
-fn print_out(str: String) {
-    let devowelized_name = remove_vowels(str.clone());
-    let total_characters = count_characters(str.clone());
-    println!("Removing vowels yields {:?}", devowelized_name);
-    println!("Total characters {:?}", total_characters);
-    println!("Original string {:?}", str);
-}
+// fn remove_vowels(name: String) -> String {
+//     let mut output = String::new();
+//     for c in name.chars() {
+//         match c {
+//             'a' | 'e' | 'i' | 'o' | 'u' => {}
+//             _ => {
+//                 output.push(c);
+//             }
+//         }
+//     }
+//     output
+// }
+
+// fn print_out(str: String) {
+//     let devowelized_name = remove_vowels(str.clone());
+//     let total_characters = count_characters(str.clone());
+//     println!("Removing vowels yields {:?}", devowelized_name);
+//     println!("Total characters {:?}", total_characters);
+//     println!("Original string {:?}", str);
+// }
